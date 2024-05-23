b1e249c7715ab51ff8719661aa5b14c8224d2c1b
Visvajit <visvajitm@gmail.com>
2024-05-23 14:30:23 -0400

    continuing forward with all my progress

--- server.js
+++ server.js
@@ -1,47 +1,40 @@
+const { Pool } = require('pg');
 const express = require('express');
-const mysql = require('mysql2');
-
 const cors = require('cors');
 
-const app = express();
-const port = process.env.PORT || 3001;
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'reactproject',
+  password: 'VMessi#10',
+  port: 5432,
+});
 
-// Middleware to parse JSON bodies
+const app = express();
 app.use(cors());
 
-app.use(express.json());
+const port = 3001; // Add this line
 
-// Create a MySQL connection
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'VMessi#10',
-  database: 'demo_app'
-});
-
-// Test MySQL connection
-connection.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
-});
-
-// Define a route to fetch data from the Customers table
 app.get('/api/customers', (req, res) => {
-    connection.query('SELECT * FROM Customers', (error, results) => {
-      if (error) throw error;
-      res.json(results);
-    });
+  pool.query('SELECT * FROM customers', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
   });
-  
-  // Define a route to fetch data from the Orders table
-  app.get('/api/orders', (req, res) => {
-    connection.query('SELECT * FROM Orders', (error, results) => {
-      if (error) throw error;
-      res.json(results);
-    });
+});
+
+app.get('/api/orders', (req, res) => {
+  pool.query('SELECT * FROM orders', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
   });
+});
 
 // Start the server
-app.listen(port, () => {
+app.listen(port, () => { // Use the port variable here
   console.log(`Server running on port ${port}`);
 });
+
